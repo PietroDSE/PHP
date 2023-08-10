2351e95ebbe110cab5d5eb8a1d9b20a9bf9cc675
Pietro d'Avila <dsescuderop@gmail.com>
2023-08-10 19:40:40 -0300

    php

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição(denovo)</title>
+</head>
+<body>
+    <h1>
+    <?php
+    echo "olá, mundo!";
+	print "\u{1F525}"
+    
+    ?>
+    </h1>
+    <p>me livrei da maldição!</p>
+</body> 
+</html>
---
+++ ex004/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /*
+    tipos primitivos em PHP:
+    ESCALARES:
+STRING
+INT/ INTEGER
+FLOAT/ DOUBLE, REAL(real deixou de existir no PHP 7)
+BOOLEAN
+    COMPOSTOS:
+
+
+
+    ESPECIAIS:x
+    */
+    ?>
+</body>
+</html>
